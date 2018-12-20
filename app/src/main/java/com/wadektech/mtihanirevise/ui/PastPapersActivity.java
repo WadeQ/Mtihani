ca492c642bd5ba5de1a5995c295c543f4fa26a15
supremos2366 <derrickwadek@gmail.com>
2018-12-20 16:51:07 +0300

    Fixed About Me Page

--- app/src/main/java/com/wadektech/mtihanirevise/ui/PastPapersActivity.java
+++ app/src/main/java/com/wadektech/mtihanirevise/ui/PastPapersActivity.java
@@ -1,6 +1,6 @@
     package com.wadektech.mtihanirevise.ui;
 
-        import android.content.Intent;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
